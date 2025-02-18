c987627db87cb5bd5b9e9e3b28991726a2cd299a
itsu-dev <itsu020402@gmail.com>
2025-02-19 08:48:16 +0900

    feat: 要素の追加

---
+++ src/main/java/dev/itsu/dom/html/HTMLPElement.java
@@ -0,0 +1,15 @@
+package dev.itsu.dom.html;
+
+import org.w3c.dom.html.HTMLParagraphElement;
+
+public class HTMLPElement extends HTMLElement implements HTMLParagraphElement {
+    @Override
+    public String getAlign() {
+        return "";
+    }
+
+    @Override
+    public void setAlign(String align) {
+
+    }
+}
---
+++ src/main/java/dev/itsu/dom/html/HTMLTextareaElement.java
@@ -0,0 +1,120 @@
+package dev.itsu.dom.html;
+
+import org.w3c.dom.html.HTMLFormElement;
+
+public class HTMLTextareaElement extends HTMLElement implements org.w3c.dom.html.HTMLTextAreaElement {
+    @Override
+    public String getDefaultValue() {
+        return "";
+    }
+
+    @Override
+    public void setDefaultValue(String defaultValue) {
+
+    }
+
+    @Override
+    public HTMLFormElement getForm() {
+        return null;
+    }
+
+    @Override
+    public String getAccessKey() {
+        return "";
+    }
+
+    @Override
+    public void setAccessKey(String accessKey) {
+
+    }
+
+    @Override
+    public int getCols() {
+        return 0;
+    }
+
+    @Override
+    public void setCols(int cols) {
+
+    }
+
+    @Override
+    public boolean getDisabled() {
+        return false;
+    }
+
+    @Override
+    public void setDisabled(boolean disabled) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public boolean getReadOnly() {
+        return false;
+    }
+
+    @Override
+    public void setReadOnly(boolean readOnly) {
+
+    }
+
+    @Override
+    public int getRows() {
+        return 0;
+    }
+
+    @Override
+    public void setRows(int rows) {
+
+    }
+
+    @Override
+    public int getTabIndex() {
+        return 0;
+    }
+
+    @Override
+    public void setTabIndex(int tabIndex) {
+
+    }
+
+    @Override
+    public String getType() {
+        return "";
+    }
+
+    @Override
+    public String getValue() {
+        return "";
+    }
+
+    @Override
+    public void setValue(String value) {
+
+    }
+
+    @Override
+    public void blur() {
+
+    }
+
+    @Override
+    public void focus() {
+
+    }
+
+    @Override
+    public void select() {
+
+    }
+}
